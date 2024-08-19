56dd185b2f35059fb698445af8a747e4f5ae0ab0
Mige <mige.kirin@gmail.com>
2024-08-18 23:31:37 -0400

    adjust burden timer

--- scripts/burden_timer.gd
+++ scripts/burden_timer.gd
@@ -6,9 +6,15 @@ func _ready() -> void:
 	wait_time = 0.5
 	timeout.connect(_timeout)
 
-func _timeout() -> void:
+func _timeout() -> void:	
 	if Global.game_started:
-		if items_spawned > 4:
-			wait_time = 3
+		if items_spawned >= 3:
+			wait_time = 2
+		if items_spawned >= 20:
+			wait_time = 1.5
+		if items_spawned >= 40:
+			wait_time = 1
+		print("objects: %s" % items_spawned)
+		print("wait time: %s" % wait_time)
 		$"../Controllers/ObjectController".launch_random_burden()
 		items_spawned += 1
