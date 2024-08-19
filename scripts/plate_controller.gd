e39261f0ffe2965e5272c9268c09d302fe75c967
Mige <mige.kirin@gmail.com>
2024-08-19 15:22:18 -0400

    change fonts

--- scripts/plate_controller.gd
+++ scripts/plate_controller.gd
@@ -6,4 +6,4 @@ func _process(_delta: float) -> void:
 	
 	Global.weight_diff = abs(left_y - right_y)
 	var percent = 100 - (Global.weight_diff / Global.max_diff * 100)
-	$Label.text = "Balance: %3.0f%%" % percent
+	$BalanceLabel.text = "%4.0f" % percent
