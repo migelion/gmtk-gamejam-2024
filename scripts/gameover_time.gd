92abcc69fa13a9071f7af698f12f3f9a73e7effc
Chris Lahey <clahey@clahey.net>
2024-08-20 09:40:40 -0400

    Elaborated on the end state.

--- scripts/gameover_time.gd
+++ scripts/gameover_time.gd
@@ -8,6 +8,6 @@ func _process(_delta: float) -> void:
 			text = "Score: %s" % timeText
 		Global.GameMode.CHARGE:
 			if Global.charge > 0:
-				text = "Finished in: %s" % timeText
+				text = "Congratulations! You were judged worthy. Welcome to Sekhet-Aaru, the Field of Reeds. The land is fertile, and you will live forever.\nYou made it through the trial in %s!" % timeText
 			else:
-				text = "Failed. Lasted: %s" % timeText
+				text = "I'm sorry. You were found wanting. Your heart has been devoured by the crocodile god, Ammit. You lasted %s before the end." % timeText
