5ddd820832873485d05076ef0edbc3a362d33392
Chris Lahey <clahey@clahey.net>
2024-08-19 23:43:50 -0400

    Hide charge bar in survival mode.

--- scripts/charge_bar.gd
+++ scripts/charge_bar.gd
@@ -3,7 +3,12 @@ extends VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.charge_changed.connect(_charge_changed)
+	match Global.game_mode:
+		Global.GameMode.SURVIVAL:
+			visible = false
+		Global.GameMode.CHARGE:
+			visible = true
+			Global.charge_changed.connect(_charge_changed)
 
 func _charge_changed(charge: float) -> void:
 	$Positive.value = charge
