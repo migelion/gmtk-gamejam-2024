8647e8a3df53459a66f20ae2cf3c39cad73138bb
Chris Lahey <clahey@clahey.net>
2024-08-20 00:19:33 -0400

    Make Burden Timer rules data based instead of code based.

--- scripts/burden_timer.gd
+++ scripts/burden_timer.gd
@@ -2,33 +2,55 @@ extends Timer
 
 var items_spawned: int = 0
 
+const SURVIVAL_WAIT_TIMES = {
+	0: 0.5,
+	3: 1.5,
+	10: 1.2,
+	18: 0.9,
+	35: 0.7,
+	80: 0.5,
+	130: 0.4,
+	180: 0.3,
+	210: 0.2
+	}
+var SURVIVAL_WAIT_TIME_KEYS = SURVIVAL_WAIT_TIMES.keys()
+
+const CHARGE_WAIT_TIMES = {
+	0: 0.5,
+	3: 1.5,
+	10: 1.2,
+	18: 0.9,
+	35: 0.7,
+	}
+var CHARGE_WAIT_TIME_KEYS = CHARGE_WAIT_TIMES.keys()
+
 func _ready() -> void:
-	wait_time = 0.5
+	SURVIVAL_WAIT_TIME_KEYS.sort()
+	CHARGE_WAIT_TIME_KEYS.sort()
+	update_wait_time()
 	timeout.connect(_timeout)
 
-const SURVIVAL_WAIT_TIMES = []
-func calculate_wait_time() -> float:
-	return 0.5
+func update_wait_time() -> void:
+	var wait_times
+	var wait_time_keys
+	match Global.game_mode:
+		Global.GameMode.SURVIVAL:
+			wait_times = SURVIVAL_WAIT_TIMES
+			wait_time_keys = SURVIVAL_WAIT_TIME_KEYS
+		Global.GameMode.CHARGE:
+			wait_times = CHARGE_WAIT_TIMES
+			wait_time_keys = CHARGE_WAIT_TIME_KEYS
+	var next_wait_time = 0.5
+	for key in wait_time_keys:
+		if key <= items_spawned:
+			next_wait_time = wait_times[key]
+		else:
+			break
+	wait_time = next_wait_time
 
 func _timeout() -> void:
 	if Global.game_started:
-		if items_spawned >= 3:
-			wait_time = 1.5
-		if items_spawned >= 10:
-			wait_time = 1.2
-		if items_spawned >= 18:
-			wait_time = 0.9
-		if items_spawned >= 35:
-			wait_time = 0.7
-		if Global.game_mode == Global.GameMode.SURVIVAL:
-			if items_spawned >= 80:
-				wait_time = 0.5
-			if items_spawned >= 130:
-				wait_time = 0.4
-			if items_spawned >= 180:
-				wait_time = 0.3
-			if items_spawned >= 210:
-				wait_time = 0.2
+		update_wait_time()
 		print("objects: %s" % items_spawned)
 		print("wait time: %s" % wait_time)
 		$"../Controllers/ObjectController".launch_random_scarab()
